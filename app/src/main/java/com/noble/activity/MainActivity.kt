1ec191dfde92a5ec1badd08945d43b13f8862750
TheVigor <igor.proskuryakov14@gmail.com>
2020-11-24 14:49:05 +0300

    simple

--- app/src/main/java/com/noble/activity/MainActivity.kt
+++ app/src/main/java/com/noble/activity/MainActivity.kt
@@ -3,10 +3,16 @@ package com.noble.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.noble.activity.ui.MinterComposeAppTheme
 
@@ -14,25 +20,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MinterComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MinterComposeApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MinterComposeApp(content: @Composable () -> Unit) {
+    MinterComposeAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = Color.Red) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(15.dp))
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Jake Wharton", "Ian Lake", "Florentina <3")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MinterComposeAppTheme {
-        Greeting("Android")
+    MinterComposeApp {
+        MyScreenContent()
     }
 }
